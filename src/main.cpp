051bb721faf643d2313e339c4d670ac06045c905
demitriaswilbert <demitriaswilbert@gmail.com>
2022-05-07 01:24:26 +0700

    added cartesian info on cout

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,21 @@
 
 int main()
 {
+    std::cout << "[-----------Demo Operator Overloading-----------]" << std::endl;
+    std::cout << "Note: " << std::endl;
+    std::cout << "      Seluruh display titik koordinat mengikuti" << std::endl;
+    std::cout << "      format sistem koordinat cartesius yaitu (x, y)" << std::endl;
+    std::cout << std::endl;
     std::cout << "[-----------------------Constructor-----------------------]" << std::endl;
+    std::cout << "Construct P0 : mid (-6, -7), panjang-x: 6, panjang-y: 6" << std::endl;
     PersegiPanjang p0 = PersegiPanjang(-6, -7, 6, 6);
-    std::cout << "P0 : " << p0 << std::endl;
+    std::cout << "          P0 : " << p0 << std::endl;
+    std::cout << "Construct P1 : mid (0, 0), panjang-x: 10, panjang-y: 20" << std::endl;
     PersegiPanjang p1 = PersegiPanjang(0, 0, 10, 20);
-    std::cout << "P1 : " << p1 << std::endl;
+    std::cout << "          P1 : " << p1 << std::endl;
+    std::cout << "Construct P2 : mid (5, 4), panjang-x: 10, panjang-y: 8" << std::endl;
     PersegiPanjang p2 = PersegiPanjang(5, 4, 10, 8);
-    std::cout << "P2 : " << p2 << std::endl;
+    std::cout << "          P2 : " << p2 << std::endl;
     std::cout << std::endl;
 
     std::cout << "[-----------------------Cek Irisan------------------------]" << std::endl;
